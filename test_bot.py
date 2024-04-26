abe0c82443eaf38a0feee07dc253de34b57d1476
Mykhailo <126985289+FiremanC4@users.noreply.github.com>
2024-04-26 17:34:53 +0300

    async echo bot

--- test_bot.py
+++ test_bot.py
@@ -3,18 +3,19 @@
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
 
-import telebot
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+
 
 # @kpktaskbot  |  https://t.me/kpktaskbot
 API_TOKEN = '7195089655:AAHVmMBUS-V_8Xm2Soz_z7GzBJ9VlIXmV9I'
-
-bot = telebot.TeleBot(API_TOKEN)
+bot = AsyncTeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
+async def send_welcome(message):
+    await bot.reply_to(message, """\
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
@@ -22,8 +23,8 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
 
-print('<<BOT STARTED>>')
-bot.infinity_polling()
+
+asyncio.run(bot.polling())
