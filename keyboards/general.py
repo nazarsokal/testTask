b14afcb1e2bae3a83e400e95b594dfbe6fdbc8ed
Mykhailo <126985289+FiremanC4@users.noreply.github.com>
2024-04-26 20:11:03 +0300

    created main keyboard

--- keyboards/general.py
+++ keyboards/general.py
@@ -1,4 +1,14 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardRemove
+
+def clear_keyboard():
+    return ReplyKeyboardRemove()
 
 def main_keyboard():
-    ...
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    # or:
+    (markup.add('Відправити запит', 'Мої запити', row_width=2)
+           .add('Підтримка')
+    )
+    # display this markup:
+    # bot.send_message(chat_id, 'Text', reply_markup=markup)
