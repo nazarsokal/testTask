224b4838d7224550901ea9619b05fd3a09c318d9
Mykhailo <126985289+FiremanC4@users.noreply.github.com>
2024-04-26 20:31:30 +0300

    fixed main keyboard function

--- keyboards/general.py
+++ keyboards/general.py
@@ -5,10 +5,9 @@ def clear_keyboard():
     return ReplyKeyboardRemove()
 
 def main_keyboard():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     # or:
-    (markup.add('Відправити запит', 'Мої запити', row_width=2)
+    (keyboard.add('Відправити запит', 'Мої запити', row_width=2)
            .add('Підтримка')
     )
-    # display this markup:
-    # bot.send_message(chat_id, 'Text', reply_markup=markup)
+    return keyboard
