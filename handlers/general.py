ba93b753456bc4c3c625904f64c450d4fdb33e6d
Mykhailo <126985289+FiremanC4@users.noreply.github.com>
2024-04-26 20:12:58 +0300

    added keyboard to the start handler

--- handlers/general.py
+++ handlers/general.py
@@ -1,6 +1,9 @@
 from dispatcher import bot
+from keyboards.general import clear_keyboard, main_keyboard 
 
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    await bot.reply_to(message, "Привіт! Я бот, щоб допомагати людям шукати допомогу")
+    await bot.reply_to(message, 
+                       "Привіт! Я бот, щоб допомагати людям шукати допомогу",
+                       reply_markup=main_keyboard())
     
