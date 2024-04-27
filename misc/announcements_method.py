bf825bbdf93199dc3300cfcfcfa11950780b2b1e
Mykhailo <126985289+FiremanC4@users.noreply.github.com>
2024-04-27 20:53:36 +0300

    comleted format_announcements_block()

--- misc/announcements_method.py
+++ misc/announcements_method.py
@@ -4,9 +4,20 @@ from i18n import (NEW_ANNOUNCEMENTT_BLOCK,
                  NEW_ANNOUNCEMENTT_BLOCK_CURRENT as NABC,
                  NEW_ANNOUNCEMENTT_BLOCK_PRESENT as NABP
                 )
-def format_announcements_block(data: dict):
+def format_announcements_block(fsm_data: dict, current_state: int):
+    if fsm_data is None: 
+        fsm_data = {}
+    
+    data = [
+        fsm_data.get('title', NABM),
+        fsm_data.get('description', NABM),
+        (NABM, NABP)[fsm_data.get('photo', False)] 
+    ]
+    data[current_state] = NABC
+    
     text = NEW_ANNOUNCEMENTT_BLOCK.format(
-        title=data.get('title', NABM),
-        description=data.get('description', NABC),
-        photo=data.get('photo', NABP)
+        title=data[0],
+        description=data[1],
+        photo=data[2]
     )
+    return text
